7fcf4c19da5de604ae5e4ce1664e8b171e20095a
Luke Swithenbank <swithenbank.luke@gmail.com>
2016-12-15 10:59:41 +1100

    update documentation and sort params alphabetically

--- spec/classes/node_exporter-version_spec.rb
+++ spec/classes/node_exporter-version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "prometheus::node_exporter" do
 
-  context 'uses the correct binary path for version < 0.12.0' do
+  context 'uses the correct binary path for version' do
     let(:facts) { {
         'operatingsystem' => 'CentOS',
         'osfamily' => 'Redhat',
@@ -15,27 +15,8 @@ describe "prometheus::node_exporter" do
 
     it do
       should contain_file('/usr/local/bin/node_exporter').with({
-        'target' => '/opt/staging/node_exporter'
+        'target' => '/opt/staging/node_exporter-0.10.0.linux-amd64/node_exporter'
       })
     end
   end
-
-  context 'uses the correct binary path for versions >= 0.12' do
-    let(:facts) { {
-        'operatingsystem' => 'CentOS',
-        'osfamily' => 'Redhat',
-        'operatingsystemrelease' => '7.0',
-        'architecture' => 'amd64',
-        'kernel' => 'Linux',
-    } }
-
-    let(:params) { {:version => '0.12.0', :arch => 'amd64', :os => 'linux'} }
-
-    it do
-      should contain_file('/usr/local/bin/node_exporter').with({
-        'target' => '/opt/staging/node_exporter-0.12.0.linux-amd64/node_exporter'
-      })
-    end
-  end
-
 end
