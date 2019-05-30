3583b096a5ec260e75b96fb7e3f03c559a5ddb0a
acassaro <acassaro@dmt.ca>
2019-05-30 09:24:45 -0500

    Minimal configuration for Alertmanager

--- spec/acceptance/alertmanager_spec.rb
+++ spec/acceptance/alertmanager_spec.rb
@@ -2,7 +2,24 @@ require 'spec_helper_acceptance'
 
 describe 'prometheus alertmanager' do
   it 'alertmanager works idempotently with no errors' do
-    pp = 'class { "prometheus::alertmanager": version => "0.17.0", extra_options => "--web.listen-address=127.0.0.1:9093"}'
+    pp = <<-EOS
+     class { 'prometheus::alertmanager':
+            version => '0.17.0',
+            extra_options => '--web.listen-address="127.0.0.1:9093"',
+            route => { 'receiver' => 'default' },
+            receivers => [
+              {
+                name => 'default',
+                email_configs => [
+                  send_resolved => true,
+                  to => 'test@localhost.localdomain',
+                  from => 'prometheus@localhost.localdomain',
+                  smarthost => '127.0.0.1:25',
+                  require_tls => false
+                ]
+              }
+            ]
+    EOS
     # Run it twice and test for idempotency
     apply_manifest(pp, catch_failures: true)
     apply_manifest(pp, catch_changes: true)
