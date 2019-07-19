cf304cf19c29d8a257b62d21c68962e2f67a3c99
Tim Meusel <tim@bastelfreak.de>
2019-07-19 13:32:25 +0200

    redis_exporter: update 0.11.2->1.0.3

--- spec/acceptance/redis_exporter_spec.rb
+++ spec/acceptance/redis_exporter_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper_acceptance'
 describe 'prometheus redis exporter' do
   it 'redis_exporter works idempotently with no errors' do
     pp = 'include prometheus::redis_exporter'
-    # Run it twice and test for idempotency
     apply_manifest(pp, catch_failures: true)
     apply_manifest(pp, catch_changes: true)
   end
@@ -12,8 +11,38 @@ describe 'prometheus redis exporter' do
     it { is_expected.to be_running }
     it { is_expected.to be_enabled }
   end
-  # the class installs an the redis_exporter that listens on port 9121
   describe port(9121) do
     it { is_expected.to be_listening.with('tcp6') }
+  end
+
+  describe 'redis_exporter update from 0.11.2 to 1.0.3' do
+    it 'is idempotent' do
+      pp = "class{'prometheus::redis_exporter': version => '0.11.2'}"
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe service('redis_exporter') do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+
+    describe port(9121) do
+      it { is_expected.to be_listening.with('tcp6') }
+    end
+    it 'is idempotent' do
+      pp = "class{'prometheus::redis_exporter': version => '1.0.3'}"
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe service('redis_exporter') do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+
+    describe port(9121) do
+      it { is_expected.to be_listening.with('tcp6') }
+    end
   end
 end
