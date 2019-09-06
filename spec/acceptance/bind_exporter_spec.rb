52e08157dfa5c4021befb80dcad4633b15a3f187
Antoine Beaupré <anarcat@debian.org>
2019-09-06 15:52:32 -0400

    bind: remove update tests as there's only one version

--- spec/acceptance/bind_exporter_spec.rb
+++ spec/acceptance/bind_exporter_spec.rb
@@ -16,21 +16,4 @@ describe 'prometheus bind exporter' do
       it { is_expected.to be_listening.with('tcp6') }
     end
   end
-
-  describe 'bind_exporter update from 0.1.0 to 0.2.0' do
-    it 'is idempotent' do
-      pp = "class{'prometheus::bind_exporter': version => '0.2.0'}"
-      apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_changes: true)
-    end
-
-    describe service('bind_exporter') do
-      it { is_expected.to be_running }
-      it { is_expected.to be_enabled }
-    end
-
-    describe port(9119) do
-      it { is_expected.to be_listening.with('tcp6') }
-    end
-  end
 end
