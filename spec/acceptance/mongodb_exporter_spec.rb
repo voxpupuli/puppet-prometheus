5976c7e28ffb9746518f797e3b98bdf48c7b86d2
Dennis Hoppe <github@debian-solutions.de>
2020-05-25 14:15:20 +0200

    Change hierarchy for acceptance tests

--- spec/acceptance/mongodb_exporter_spec.rb
+++ spec/acceptance/mongodb_exporter_spec.rb
@@ -1,23 +1,15 @@
 require 'spec_helper_acceptance'
 
-describe 'prometheus mongodb exporter' do
+describe 'prometheus mongodb_exporter' do
   it 'mongodb_exporter works idempotently with no errors' do
     pp = 'include prometheus::mongodb_exporter'
     apply_manifest(pp, catch_failures: true)
     apply_manifest(pp, catch_changes: true)
   end
 
-  describe service('mongodb_exporter') do
-    it { is_expected.to be_running }
-    it { is_expected.to be_enabled }
-  end
-  describe port(9216) do
-    it { is_expected.to be_listening.with('tcp6') }
-  end
-
-  describe 'mongodb_exporter update from 0.3.1 to 0.11.0' do
-    it 'is idempotent' do
-      pp = "class{'prometheus::mongodb_exporter': version => '0.3.1'}"
+  describe 'prometheus mongodb_exporter version 0.3.1' do
+    it ' mongodb_exporter installs with version 0.3.1' do
+      pp = "class { 'prometheus::mongodb_exporter': version => '0.3.1' }"
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
     end
@@ -30,8 +22,11 @@ describe 'prometheus mongodb exporter' do
     describe port(9216) do
       it { is_expected.to be_listening.with('tcp6') }
     end
-    it 'is idempotent' do
-      pp = "class{'prometheus::mongodb_exporter': version => '0.11.0'}"
+  end
+
+  describe 'prometheus mongodb_exporter version 0.11.0' do
+    it ' mongodb_exporter installs with version 0.11.0' do
+      pp = "class { 'prometheus::mongodb_exporter': version => '0.11.0' }"
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
     end
