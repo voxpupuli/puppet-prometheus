fb8778112cf69384cd85a80a521df24eed79e510
Christoph Buchenberg <christoph.buchenberg@qbees.io>
2022-08-02 13:08:35 +0200

    confine to linux

--- lib/facter/alert_manager_running.rb
+++ lib/facter/alert_manager_running.rb
@@ -3,10 +3,13 @@
 require 'puppet'
 
 Facter.add('prometheus_alert_manager_running') do
+  confine kernel: :linux
   setcode do
-    service = Puppet::Type.type(:service).new(:name => 'alert_manager') # rubocop:disable Style/HashSyntax
-    service.provider.status == :running
-  rescue Puppet::Error
-    false
+    begin
+      service = Puppet::Type.type(:service).new(:name => 'alert_manager') # rubocop:disable Style/HashSyntax
+      service.provider.status == :running
+    rescue Puppet::Error
+      false
+    end
   end
 end
