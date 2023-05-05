271706c3120cabb0b7233b61a05e8370c3b2c5dd
stevegreengvl <119502600+stevegreengvl@users.noreply.github.com>
2023-05-05 16:47:43 +0200

    Update rabbitmq_exporter.pp

--- manifests/rabbitmq_exporter.pp
+++ manifests/rabbitmq_exporter.pp
@@ -109,9 +109,9 @@ class prometheus::rabbitmq_exporter (
   Optional[Enum['none', 'http', 'https', 'ftp']] $proxy_type = undef,
 ) inherits prometheus {
   if versioncmp($version, '0.9') < 0 {
-    $real_download_url = pick($download_url, "${download_url_base}/download/v${version}/${package_name}_${release}.${os}-${arch}.${download_extension}")
+    $real_download_url    = pick($download_url, "${download_url_base}/download/v${version}/${package_name}-${version}.${os}-${arch}.${download_extension}")
   } else {
-    $real_download_url = pick($download_url, "${download_url_base}/download/v${version}/${package_name}-${version}_${os}-${arch}.${download_extension}")
+    $real_download_url    = pick($download_url, "${download_url_base}/download/v${version}/${package_name}-${version}_${os}-${arch}.${download_extension}")
   }
   $notify_service = $restart_on_change ? {
     true    => Service[$service_name],
