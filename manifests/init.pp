10d07f81a0ec4d3ac20763bfa3b1af111382f7ca
Francis Begyn <francis@begyn.be>
2023-11-22 16:24:22 +0100

    fix wording to match prometheus config wording

--- manifests/init.pp
+++ manifests/init.pp
@@ -75,7 +75,7 @@
 #  Prometheus remote_write config to scrape prometheus 1.8+ instances
 # @param enable_tracing
 #  Prometheus enables experimental tracing in Prometheus config file
-# @param tracing_config
+# @param tracing_configs
 #  Prometheus tracing configuration for the Prometheus config file
 # @param alerts
 #  alert rules to put in alerts.rules
@@ -235,7 +235,7 @@ class prometheus (
   Array $remote_read_configs = [],
   Array $remote_write_configs = [],
   Boolean $enable_tracing,
-  Hash $tracing_config,
+  Hash $tracing_configs,
   Stdlib::Absolutepath $shared_dir = '/usr/local/share/prometheus',
   String $storage_retention = '360h',
   String $user = 'prometheus',
