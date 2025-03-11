3c8b0fe00c78608055be75155cd51b881940a732
Evan <41494790+evaan@users.noreply.github.com>
2025-03-11 15:30:42 -0230

    Remove epps

---
+++ files/update_metrics.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while IFS= read -r line || [[ -n "$line" ]]; do
+  [[ -z "$line" ]] && continue
+  key="${line%%=*}"
+  value="${line#*=}"
+  output=$(eval "$value" 2>/dev/null)
+  echo "$key $output" >> "$2/active.prom.$$"
+done < $1
+
+if [ -f "$2/active.prom.$$" ]; then
+  mv "$2/active.prom.$$" "$2/active.prom"
+else
+  rm -f "$2/active.prom"
+fi
